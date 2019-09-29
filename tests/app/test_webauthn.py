6ceba4a5df02472df10144a050cb068d48eb1a36
Radoslav Bodo <bodik@cesnet.cz>
2019-09-29 19:46:01 +0000

    webauthn: authentication tests

--- tests/app/test_webauthn.py
+++ tests/app/test_webauthn.py
@@ -9,6 +9,7 @@ from soft_webauthn import SoftWebauthnDevice
 
 from fwe import webauthn
 from fwe.models import User, WebauthnCredential
+from tests import persist_and_detach
 from tests.app import get_csrf_token
 
 
@@ -72,3 +73,65 @@ def test_webauthn_register_route_invalid_attestation(cl_user):
     response = form.submit()
     assert response.status_code == HTTPStatus.OK
     assert response.lxml.xpath('//script[contains(text(), "toastr[\'error\'](\'Error during registration.\');")]')
+
+
+def test_webauthn_login_route(client, test_user):
+    """test login by webauthn"""
+
+    device = SoftWebauthnDevice()
+    device.cred_init(webauthn.rp.ident, b'randomhandle')
+    persist_and_detach(WebauthnCredential(
+        user=test_user,
+        user_handle=device.user_handle,
+        credential_data=cbor.encode(device.cred_as_attested().__dict__)))
+
+    form = client.get(url_for('app.login_route')).form
+    form['username'] = test_user.username
+    response = form.submit()
+    assert response.status_code == HTTPStatus.FOUND
+
+    response = response.follow()
+    # some javascript code muset be emulated
+    pkcro = cbor.decode(b64decode(client.post(url_for('app.webauthn_pkcro_route'), {'csrf_token': get_csrf_token(client)}).body))
+    assertion = device.get(pkcro, 'https://%s' % webauthn.rp.ident)
+    assertion_data = {
+        'credentialRawId': assertion['rawId'],
+        'authenticatorData': assertion['response']['authenticatorData'],
+        'clientDataJSON': assertion['response']['clientDataJSON'],
+        'signature': assertion['response']['signature'],
+        'userHandle': assertion['response']['userHandle']}
+    form = response.form
+    form['assertion'] = b64encode(cbor.encode(assertion_data))
+    response = form.submit()
+    # and back to standard test codeflow
+    assert response.status_code == HTTPStatus.FOUND
+
+    response = client.get(url_for('app.index_route'))
+    assert response.lxml.xpath('//a[text()="Logout"]')
+
+
+def test_webauthn_pkcro_route_invalid_request(client):
+    """test error handling in pkcro route"""
+
+    response = client.post(url_for('app.webauthn_pkcro_route'), status='*')
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_webauthn_invalid_assertion(client, test_wncred):
+    """test login by webauthn; error hanling"""
+
+    response = client.get(url_for('app.webauthn_login_route'))
+    assert response.status_code == HTTPStatus.FOUND
+    assert url_for('app.login_route') in response.headers['Location']
+
+    form = client.get(url_for('app.login_route')).form
+    form['username'] = User.query.get(test_wncred.user_id).username
+    response = form.submit()
+    assert response.status_code == HTTPStatus.FOUND
+
+    response = response.follow()
+    form = response.form
+    form['assertion'] = 'invalid'
+    response = form.submit()
+    assert response.status_code == HTTPStatus.OK
+    assert response.lxml.xpath('//script[contains(text(), "toastr[\'error\'](\'Login error during Webauthn authentication.\');")]')
