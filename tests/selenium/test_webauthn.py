4c778eadaab2112b793f07cb874bffd5c4325400
Radoslav Bodo <bodik@cesnet.cz>
2019-09-29 20:00:17 +0000

    webauthn: authentication selenium tests

--- tests/selenium/test_webauthn.py
+++ tests/selenium/test_webauthn.py
@@ -4,11 +4,14 @@ from base64 import b64decode, b64encode
 
 from fido2 import cbor
 from flask import url_for
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from soft_webauthn import SoftWebauthnDevice
 
 from fwe import webauthn
-from fwe.models import User
+from fwe.models import User, WebauthnCredential
+from tests import persist_and_detach
 from tests.selenium import js_variable_ready, WEBDRIVER_WAIT
 
 
@@ -29,3 +32,27 @@ def test_webauthn_register_route(live_server, sl_user):  # pylint: disable=unuse
 
     user = User.query.filter(User.username == 'pytest_user').one()
     assert user.webauthn_credentials
+
+
+def test_login_webauthn(live_server, selenium, test_user):  # pylint: disable=unused-argument
+    """test login by webauthn"""
+
+    device = SoftWebauthnDevice()
+    device.cred_init(webauthn.rp.ident, b'randomhandle')
+    persist_and_detach(WebauthnCredential(
+        user_handle=device.user_handle,
+        user=test_user,
+        credential_data=cbor.encode(device.cred_as_attested().__dict__)))
+
+    selenium.get(url_for('app.login_route', _external=True))
+    selenium.find_element_by_xpath('//form//input[@name="username"]').send_keys(test_user.username)
+    selenium.find_element_by_xpath('//form//input[@type="submit"]').click()
+
+    # some javascript code must be emulated
+    WebDriverWait(selenium, WEBDRIVER_WAIT).until(js_variable_ready('window.pkcro_raw'))
+    pkcro = cbor.decode(b64decode(selenium.execute_script('return window.pkcro_raw;').encode('utf-8')))
+    assertion = device.get(pkcro, 'https://%s' % webauthn.rp.ident)
+    selenium.execute_script('authenticate_assertion(CBOR.decode(base64_to_array_buffer("%s")));' % b64encode(cbor.encode(assertion)).decode('utf-8'))
+    # and back to standard test codeflow
+
+    WebDriverWait(selenium, WEBDRIVER_WAIT).until(EC.presence_of_element_located((By.XPATH, '//a[text()="Logout"]')))
