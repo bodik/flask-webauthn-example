30d46ed68137751a3b6c9a26f4c85a2e6f86ec61
Radoslav Bodo <bodik@cesnet.cz>
2019-09-29 10:20:19 +0000

    webauthn: registration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 """fwe common pytest fixtures"""
 
 import pytest
+from fido2 import cbor
+from soft_webauthn import SoftWebauthnDevice
 
-from fwe import create_app, db
+from fwe import create_app, db, webauthn
 from fwe.commands import db_remove
-from fwe.models import User
+from fwe.models import User, WebauthnCredential
 from fwe.password_supervisor import PasswordSupervisor as PWS
 from tests import persist_and_detach
 
@@ -25,3 +27,18 @@ def app():
 def test_user(app):  # pylint: disable=unused-argument,redefined-outer-name
     """persistent test user"""
     return persist_and_detach(User(username='user1', password=PWS().generate()))
+
+
+@pytest.fixture
+def test_wncred(test_user):  # pylint: disable=redefined-outer-name
+    """persistent test registered webauthn credential"""
+
+    device = SoftWebauthnDevice()
+    device.cred_init(webauthn.rp.ident, b'randomhandle')
+    wncred = WebauthnCredential(
+        user_id=test_user.id,
+        user=test_user,
+        user_handle=device.user_handle,
+        credential_data=cbor.encode(device.cred_as_attested().__dict__),
+        name='testcredential')
+    yield persist_and_detach(wncred)
